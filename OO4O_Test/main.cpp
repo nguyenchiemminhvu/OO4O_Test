0f99db94ab2063a925aa3ad831809d595c911ee9
NCMV <nguyenchiemminhvu@gmail.com>
2020-03-26 13:49:06 +0700

    update

---
+++ OO4O_Test/main.cpp
@@ -0,0 +1,33 @@
+#include <oracl.h>
+
+using namespace System;
+
+#define DEBUG Console::Write
+#define DEBUG_LINE Console::WriteLine
+
+int main()
+{
+	OStartup();
+
+	ODatabase noncondb;
+	oresult res = noncondb.Open("localhost:1521/NONCONDB", "HR", "HR");
+	if (res != OSUCCESS)
+	{
+		DEBUG_LINE("Can not open NONCONDB database");
+		return -1;
+	}
+
+	ODynaset allEmp(noncondb, "select * from employees");
+	double salary = 0.0f;
+	while (!allEmp.IsEOF())
+	{
+		allEmp.GetFieldValue("salary", &salary);
+		
+		DEBUG_LINE(salary);
+
+		allEmp.MoveNext();
+	}
+
+	OShutdown();
+	return 0;
+}
