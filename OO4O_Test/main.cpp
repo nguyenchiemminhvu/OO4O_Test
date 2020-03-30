bc14962b0c01e67706953c25eb3f63d6c7ef8ccf
NCMV <nguyenchiemminhvu@gmail.com>
2020-03-30 14:24:16 +0700

    update

--- OO4O_Test/main.cpp
+++ OO4O_Test/main.cpp
@@ -4,25 +4,13 @@
 
 #include <oracl.h>
 #include <obound.h>
-#include <omfc.h>
-
-using namespace System;
-using namespace System::Runtime::InteropServices;
 
 int main()
 {
 	OStartup();
 	
-	ODatabase db("localhost:1521/NONCONDB", "HR", "HR");
-	ODynaset ds(db, "select sum(SALARY) as sumsal from EMPLOYEES");
-	ds.MoveFirst();
-	if (!ds.IsEOF())
-	{
-		double s;
-		ds.GetField("sumsal").GetValue(&s);
-		std::cout << s << std::endl;
-	}
 	
+
 	OShutdown();
 	return 0;
 }
